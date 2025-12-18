541681cddab91bd75fde423fad4a8d77fead2dee
Arthur-018 <toledoarthur11@gmail.com>
2025-12-18 11:26:59 -0300

    Add files via upload

--- Estudo-Aula/funcoes.js
+++ Estudo-Aula/funcoes.js
@@ -1,11 +1,26 @@
-function saudacao(nome){
+/*function saudacao(nome){
     console.log(`Olá ${nome}`)
-}
+}*/
+
+/*function calcularDobro(numero){
+    numero *2
+}*/
+
+//const saudacao = (nome) => {
+ //   console.log(`Vida longa e próspera ${nome}`)
+ //}
+
+ //const calcularDobro = (numero) => {
+//    return numero * 2;
+//}
+
+const saudacao = nome => console.log(`Olá, ${nome}`)
 
 saudacao("Arthur")
 
-function calcularDobro(numero){
-    numero *2
-}
+const calcularDobro = numero => numero * 2
 
-calcularDobro(2)
+
+const numeroDobrado = calcularDobro(5)
+console.log(`O dobro de 5 é ${numeroDobrado}`)
+
