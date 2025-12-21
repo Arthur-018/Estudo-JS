39c0074c871453ba574c5c737a672aa3dfbfa806
Arthur Toledo <toledoarthur11@gmil.com>
2025-12-20 21:12:13 -0300

    Aula sobre Entrada de Terminal

--- Estudo-Aula/funcoesGlobais.js
+++ Estudo-Aula/funcoesGlobais.js
@@ -3,3 +3,14 @@ function saudacao(){
 }
 
 setTimeout(saudacao, 5000) // ms
+
+let contador = 0;
+
+const id = setInterval(() => {
+    contador++
+    console.log(`Tempo decorrido ${contador}`)
+
+    if(contador == 10){
+        clearInterval(id)
+    }
+}, 1000)
