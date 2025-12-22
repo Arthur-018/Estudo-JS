9b29ca278ba8f1b26a2b1fae7a69e9c0975045aa
Arthur Toledo <toledoarthur11@gmil.com>
2025-12-22 14:32:53 -0300

    Arrumando as pastas

---
+++ Estudo-Aula/Funções/callbackHOF.js
@@ -0,0 +1,26 @@
+//HOF -> Higher-Order- Function === função que recebe outra função como parametro
+
+
+function calcular(numero1, numero2, operacao) {
+
+    return operacao(numero1, numero2)
+}
+
+function soma(numero1, numero2) {
+    return numero1 + numero2
+}
+
+function divisao(numero1, numero2) {
+    return numero1 / numero2
+}
+
+function multiplicacao(numero1, numero2) {
+    return numero1 * numero2
+}
+
+const resultadoSoma = calcular(3, 8, soma)
+console.log(`Resultado da soma: ${resultadoSoma}`);
+
+const resultadoDivisao = calcular(12, 6, divisao)
+console.log(`O resultado da divisão é: ${resultadoDivisao}`);
+
