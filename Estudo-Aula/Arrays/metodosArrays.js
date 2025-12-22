0eaf1356fa27f496b65794c80e47c497b9efd7ed
Arthur Toledo <toledoarthur11@gmil.com>
2025-12-22 15:54:21 -0300

    Arrays

---
+++ Estudo-Aula/Arrays/metodosArrays.js
@@ -0,0 +1,21 @@
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const numeroPares = numeros.filter((numero) => {
+    return numero % 2 === 0
+})
+
+const numeroImpares = numeros.filter((numero) => {
+    return numero % 2 != 0
+})
+
+console.log('Todoso os números', numeros)
+
+console.log('Todos os números pares: ', numeroPares)
+
+console.log('Todos os números impares ', numeroImpares)
+
+const numerosDobrados = numeros.map((numero) =>{
+    return numero * 2
+})
+
+console.log('Nossa lista mapeada: ', numerosDobrados)
