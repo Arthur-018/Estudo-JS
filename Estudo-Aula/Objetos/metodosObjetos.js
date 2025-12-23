8e7453e43dc68960d09958de711cf1bf73355e5f
Arthur-018 <toledoarthur11@gmail.com>
2025-12-23 07:53:07 -0300

    Add files via upload

--- Estudo-Aula/Objetos/metodosObjetos.js
+++ Estudo-Aula/Objetos/metodosObjetos.js
@@ -10,3 +10,15 @@ for(const chave in pessoa){
      console.log('Valor: ', pessoa[chave])
 
 }
+
+console.log('====================================')
+
+const chaves = Object.keys(pessoa)
+const valores = Object.values(pessoa)
+
+
+const entradas = Object.entries(pessoa)
+
+console.log('Chaves: ', chaves)
+console.log('Valores: ', valores)
+console.log('Entradas (chave/valor)): ', entradas) 
