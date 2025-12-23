ebdb1ddb8d2cf23d0efefe5960ba501c2d2bbbf9
Arthur-018 <toledoarthur11@gmail.com>
2025-12-23 09:30:48 -0300

    Add files via upload

---
+++ Estudo-Aula/Datas/datas.js
@@ -0,0 +1,22 @@
+const agora = new Date()
+
+console.log(agora)
+
+console.log('Ano: ', agora.getFullYear);
+console.log('Mês 0-11: ', agora.getMonth);
+console.log('Dia do mês: ', agora.getDate);
+console.log('Hora: ', agora.getHours);
+console.log('Minutos: ', agora.getMinutes);
+
+
+
+const nascimento = new Date(2007, 1, 16)
+
+console.log(nascimento)
+
+console.log('Data formatada (BR): ', nascimento.toLocaleDateString('pr-BR'))
+console.log('Data formatada (US): ', nascimento.toLocaleDateString('en-US'))
+
+
+
+
