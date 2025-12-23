bbcb3c73265089c10332ea6ab7862ae514faf00f
Arthur-018 <toledoarthur11@gmail.com>
2025-12-23 09:14:54 -0300

    Add files via upload

--- Exercicios/Modulo-Dez/exercicios.js
+++ Exercicios/Modulo-Dez/exercicios.js
@@ -1,7 +1,7 @@
 //Exercício 1
 const objeto = {
     nome: 'Arthur',
-    idade: 18, 
+    idade: 18,
     profissao: 'Desenvolvedor Full Stack'
 }
 
@@ -17,16 +17,85 @@ objeto.idade = 19;
 //Exercício 4
 objeto.cidade = 'Porto Alegre';
 
+
+
+
+//Exercício 5
 const pessoa = {
     nome: 'André',
     idade: 20,
     nacionalidade: "Brasileiro"
 }
-function objetos(pessoa){
-    console.log(pessoa.nome)
-    console.log(pessoa.idade)
-    console.log(pessoa.nacionalidade)
+function objetos(pessoa) {
+    console.log(pessoa)
 }
 objetos(pessoa)
 
-const lista =
+
+
+//Exercício 6
+const objetoUm = {
+    nome: 'Arthur',
+    idade: 18,
+
+}
+const objetoDois = {
+    nome: 'Danilo',
+    idade: 14
+}
+const objetoTres = {
+    nome: 'Gabriel',
+    idade: 20
+}
+
+const lista = [objetoUm, objetoDois, objetoTres]
+
+console.log('Lista: ', lista)
+
+
+//Exercício 7
+
+for (let l of lista) { 
+
+if (l.idade >= 18) { 
+
+console.log(`${l.nome} é maior de idade.`); 
+
+} 
+
+} 
+
+
+
+//Exercício 8
+ 
+const usuario = {
+    nome: 'Arthur',
+    saudacao: function(){
+        console.log(`Olá, eu sou ${this.nome}`)
+    }
+}
+
+usuario.saudacao()
+
+
+
+//Exercício 9
+for(const chave in objeto){
+    console.log('Chave: ', chave)
+    console.log('Valor: ', objeto[chave]);
+    
+}
+
+
+//Exercício 10
+let produto = {
+    preco: 10,
+    quantidade: 2
+}   
+
+const resultado = produto.preco * produto.quantidade;
+console.log('Valor total da compra: ', resultado)
+
+
+  
