4d9f0b4e899b2900bc71e156914378dee5ac4099
Arthur-018 <toledoarthur11@gmail.com>
2026-01-19 09:07:39 -0300

    Add files via upload

--- JavaScript conhecendo objetos/Exercicios/infoPessoa.js
+++ JavaScript conhecendo objetos/Exercicios/infoPessoa.js
@@ -3,16 +3,27 @@ const pessoa = {
     idade: 18,
     solteiro: false,
     hobbies: ["Jogar Resident Evil", " Estudar"],
+    endereco: {
+        rua: 'Rua Gilberto',
+        cidade: 'Porto Alegre',
+        estado: 'RS'
+    },
+
 };
 
-function mostrarInfoPessoa (pessoa){
+function mostrarInfoPessoa(pessoa) {
     console.log('Informações pessoa:');
     console.log(`Nome: ${pessoa.nome}  (Tipo: ${typeof pessoa.nome})`);
     console.log(`Idade: ${pessoa.idade} (Tipo: ${typeof pessoa.idade})`);
     console.log(`Solteiro: ${pessoa.solteiro} (Tipo: ${typeof pessoa.solteiro})`);
     console.log(`Hobbies: ${pessoa.hobbies} (Tipo: ${typeof pessoa.hobbies})`);
-    
-    
+    console.log(`Rua: ${pessoa.endereco.rua}`);
+    console.log(`Rua: ${pessoa.endereco.cidade}`);
+    console.log(`Rua: ${pessoa.endereco.estado}`);
+
+
 }
-    
+
+
+
 console.log(mostrarInfoPessoa(pessoa));
