da57279c5f9d88c93e6a6cca06d1544a931ddeea
Arthur-018 <toledoarthur11@gmail.com>
2026-01-20 08:08:28 -0300

    Add files via upload

--- JavaScript conhecendo objetos/Exercicios/calculadora.js
+++ JavaScript conhecendo objetos/Exercicios/calculadora.js
@@ -9,6 +9,18 @@ const calculadora = {
         return a * b
     },
     divisao: function (a, b){
-        return a /  b
+        return a / b
     },
+    calcularMedia: function(numeros){
+        const soma = numeros.reduce((total, numero) => total + numero, 0);
+        return soma / numeros.length 
+    }
 }
+const resultado = calculadora.soma(6, 2);
+
+console.log(resultado);
+
+const numerosParaMedia = [10, 8, 6, 9, 7];
+console.log("Média: " + calculadora.calcularMedia(numerosParaMedia));
+
+
---
+++ JavaScript conhecendo objetos/Exercicios/contaBancaria.js
@@ -0,0 +1,30 @@
+const contaBancaria = {
+      titular: "Arthur",
+    saldo: 700,
+    depositar: function(valor) {
+        this.saldo += valor;
+    },
+    sacar: function(valor) {
+        if (valor <= this.saldo) {
+            this.saldo -= valor;
+        } else {
+            console.log("Saldo insuficiente para saque.");
+        }
+    }
+
+}
+
+const cliente = {
+    nome: 'André',
+    conta: contaBancaria
+}
+
+function mostrarSaldo(cliente) {
+    console.log(`Nome do cliente: ${cliente.nome}`);
+    console.log(`Saldo da conta: ${cliente.conta.saldo}`);
+}
+
+cliente.conta.depositar(500);
+cliente.conta.sacar(200);
+
+mostrarSaldo(cliente)
