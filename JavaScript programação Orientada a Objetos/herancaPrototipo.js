561e70b0b4f3fffd649c00e767249cda9b2151e8
Arthur-018 <toledoarthur11@gmail.com>
2026-01-21 09:15:16 -0300

    Add files via upload

--- JavaScript programação Orientada a Objetos/herancaPrototipo.js
+++ JavaScript programação Orientada a Objetos/herancaPrototipo.js
@@ -1,25 +1,25 @@
 const user = {
-    nome: 'Juliana',
-    email: 'j@j.com',
-    nascimento: '2024-01-01',
-    role: 'estudante',
-    ativo: true,
-    exibirInfos: function(){
-        console.log(this.nome, this.email);
-    },
-}
+  nome: "Juliana",
+  email: "j@j.com",
+  nascimento: "2024-01-01",
+  role: "estudante",
+  ativo: true,
+  exibirInfos: function () {
+    console.log(this.nome, this.email);
+  },
+};
 
 const admin = {
-    nome: 'Mariana',
-    email: 'm@m.com',
-    nascimento: '2024-01-01',
-    role: 'admin',
-    ativo: true,
-    criarCurso: function(){
-        console.log('curso criado');
-    }
+  nome: "Mariana",
+  email: "m@m.com",
+  nascimento: "2024-01-01",
+  role: "admin",
+  ativo: true,
+  criarCurso: function() {
+    console.log('curso criado');
+  }
 }
 
-Object.setPrototypeOf(admin, user) //Primeiro parâmetro é o que pega, segundo parâmetro é o Objeto que fornece a base
+Object.setPrototypeOf(admin, user)
 admin.criarCurso()
-admin.exibirIfos()
+admin.exibirInfos()
