c744264b8403380d3e71ea4ea4a4e0d0df8e3462
Arthur-018 <toledoarthur11@gmail.com>
2026-01-21 11:40:09 -0300

    Add files via upload

--- JavaScript com Node.js criando sua primeira biblioteca/src/index.js
+++ JavaScript com Node.js criando sua primeira biblioteca/src/index.js
@@ -4,5 +4,25 @@ const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2]; 
 
 fs.readFile(link, 'utf-8', (erro, texto) => {
-  console.log(texto);
+  verificaPalavraDuplicadas(texto);
 })
+
+//Criar um array com as palavras
+//contar as ocorrências
+//montar um objeto com o resultado
+
+//{
+//  "web": 5,
+//  "computador": 4
+//}
+
+function verificaPalavraDuplicadas(texto){
+    const listaPalvaras = texto.split(' ');
+    const resultado = {};
+    //objeto[propriedade] = valor;
+    listaPalvaras.forEach(palavra => {
+        resultado[palavra] = (resultado[palavra] || 0) + 1
+    })
+    console.log(resultado);
+    
+}
