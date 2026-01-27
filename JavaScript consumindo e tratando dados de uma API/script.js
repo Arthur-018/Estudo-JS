49af98e6af50392ef3277d3e9941730f302ce5a9
Arthur-018 <toledoarthur11@gmail.com>
2026-01-27 10:44:50 -0300

    Add files via upload

--- JavaScript consumindo e tratando dados de uma API/script.js
+++ JavaScript consumindo e tratando dados de uma API/script.js
@@ -1,1 +1,12 @@
-console.log('Hello world');
+consultaCep = fetch('https://viacep.com.br/ws/91510090/json/')
+.then(resposta => resposta.json())
+.then(r => {
+    if(r.erro){
+        throw Error('Esse cep não existe')
+    }
+    console.log(r)})
+.catch(erro => console.log(erro))
+.finally(mensagem => console.log('Processamento concluído!')
+);
+
+console.log(consultaCep);
